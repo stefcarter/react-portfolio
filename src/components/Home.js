bc9a1e7bcf73787ec9dd5da1dafda902f71dac28
Steffan Carter <xisled09@gmail.com>
2022-08-04 00:46:20 -0400

    styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 function Home() {
     return (
-      <div>
-        <h1>Filler test</h1>
-        <p>Even more filler test</p>
-      </div>
-    )
+        <div className="card bg-white card-rounded w-100 min-100-vh">
+          <div className="card-header bg-dark text-center">
+            <h1>Welcome to </h1>
+          </div>
+          <div className="card-body m-5">
+            <h2 className="s-50">Welcome to my page, my name is Steffan Carter</h2>
+            <h2>Building the life of my dreams from the ground up.</h2>
+            <h2 className="s-50">The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</h2>
+    
+            <div className="text-center">
+              examples place holder
+            </div>
+          </div>
+        </div>
+      );
 }
   
   
